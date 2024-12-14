26dc6ca651f89520700a2bafbf649be413ea25e9
barbaraeguche <barbaraeguche@icloud.com>
2024-12-14 13:49:40 -0500

    nothing i can do currently, will work to move backend to vercel also

--- client/src/util/axiosConfig.ts
+++ client/src/util/axiosConfig.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // use environment variable for flexibility
-// const backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://127.0.0.1:5000';
-const backendUrl = 'http://127.0.0.1:5000'; // comment the above, and uncomment this when working locally to avoid access control issues
+const backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://127.0.0.1:5000';
+// const backendUrl = 'http://127.0.0.1:5000'; // comment the above, and uncomment this when working locally to avoid access control issues
 
 export const apiClient = axios.create({
 	baseURL: `${backendUrl}/grocery`,
