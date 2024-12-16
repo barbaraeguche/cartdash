411fa5f0de0e9e2c0669f940a2a95530ae232e23
barbara 💌 <110749315+barbaraeguche@users.noreply.github.com>
2024-12-16 13:02:56 -0500

    update header.tsx
    remove emoji from h1 tag

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
 	return (
 		<header className="flex flex-col text-center mt-10">
 			<h1 className="font-luckiest-guy text-[40px] tracking-[15%] sm:tracking-[30%] md:tracking-[50%] text-mint-100">
-				cartdash🧺
+				cartdash
 			</h1>
 			<span className="italic text-[15px] sm:ml-16">
 				simplifying your grocery shopping experience...
